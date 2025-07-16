1f84666eab8c45aba85bf153b2433770ad8903a8
k.bashlykov <qSatan1s@yandex.ru>
2025-07-16 08:50:52 +0500

    конфликт vite config с верселом

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/main-variables.scss" as *;`,
+      },
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
+}) 
